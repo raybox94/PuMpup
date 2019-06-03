cf20cb4d760d45d9dd6da3eedab98a3ff55cdc51
rayaan <rayaan@rayaans-air.nwmissouri.edu>
2019-06-03 10:52:26 -0500

    update code

--- script.js
+++ script.js
@@ -4,21 +4,44 @@ Git repository link: https://github.com/raybox94/PuMpup
 
 */
 
+var knowledge_area_arr = ["Project Integration", "Project Scope", "Project Schedule", "Project Cost", "Project Quality", "Project Resource", "Project Communications", "Project Risk", "Project Procurement", "Project Stakeholder"];
+var process_group_arr = ["Initiating", "Planning", "Execution", "Monitoring and controlling", "Closing"];
+
+
 function knowledge_area(){
+var count = 0;
 
  var a = document.getElementById("sortable").innerHTML;
 
 var clean_data = strip_string_knowledge(a);
 console.log(clean_data);
 
+for(var k = 0; k < clean_data.length; k++){
+if(clean_data[k] == knowledge_area_arr[k]){
+count += 1;
+}//end of if statement
+
+}//end of for loop
+console.log(count);
+document.getElementById("score").innerHTML = count;
+
 }//end of function knowledge_area
 
 function process_group(){
-
+var count = 0;
  var b = document.getElementById("sortable").innerHTML;
 
 var clean_data = strip_string_process(b);
 console.log(clean_data);
+
+for(var p = 0; p < clean_data.length; p++){
+if(clean_data[p] == process_group_arr[p]){
+count += 1;
+}//end of if statement
+
+}//end of for loop
+console.log(count);
+document.getElementById("score").innerHTML = count;
 
 
 }//end of function process_group
